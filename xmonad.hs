843a7c6b574bdc7315cbf0bbde79e525b89de5c2
Alexander frenzel <alex@relatedworks.com>
2011-03-12 11:59:44 +0100

    blubb

--- xmonad.hs
+++ xmonad.hs
@@ -1,152 +1,51 @@
--- Import stuff
+-- XMonad modules
 import XMonad
-import qualified XMonad.StackSet as W 
-import qualified Data.Map as M
-import XMonad.Util.EZConfig(additionalKeys)
-import System.Exit
-import Graphics.X11.Xlib
-import System.IO
- 
- 
--- actions
-import XMonad.Actions.CycleWS
-import XMonad.Actions.WindowGo
-import qualified XMonad.Actions.Search as S
-import XMonad.Actions.Search
-import qualified XMonad.Actions.Submap as SM
-import XMonad.Actions.GridSelect
- 
--- utils
-import XMonad.Util.Scratchpad (scratchpadSpawnAction, scratchpadManageHook, scratchpadFilterOutWorkspace)
-import XMonad.Util.Run(spawnPipe)
-import qualified XMonad.Prompt 		as P
-import XMonad.Prompt.Shell
-import XMonad.Prompt
- 
- 
--- hooks
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.UrgencyHook
-import XMonad.Hooks.ManageHelpers
- 
--- Data.Ratio for IM layout
-import Data.Ratio ((%))
+import XMonad.Hooks.DynamicLog
 
--- Custom
- 
+-- Keybindings , greedyview 'n stuff
+import XMonad.Util.EZConfig
+
+-- spawnPipe and hPutStrLn
+import XMonad.Util.Run
+
+-- urgent notifications
+import XMonad.Hooks.UrgencyHook
+
+-- Custom Rules
+import Workspaces
+import LayoutRules
+import ApplictionRules
+import Tools 
  
 -- Main --
 main = do
         xmproc <- spawnPipe "xmobar"  -- start xmobar
     	xmonad 	$ withUrgencyHook NoUrgencyHook $ defaultConfig
         	{ manageHook = myManageHook
-        	, layoutHook = myLayoutHook  
-		, borderWidth = myBorderWidth
-		, normalBorderColor = myNormalBorderColor
-		, focusedBorderColor = myFocusedBorderColor
+        	, layoutHook = LayoutRules.layoutRules
+		, logHook = myLogHook
+
+		, modMask = mod4mask
 		, keys = myKeys
-		, logHook = myLogHook xmproc
-        	, modMask = myModMask  
-        	, terminal = myTerminal
-		, workspaces = myWorkspaces
+		, terminal = "urxvt"
+
+		, borderWidth = 1 
+		, normalBorderColor = "#333333"
+		, focusedBorderColor = "#FF0000" 
+
+		, workspaces = Workspaces.myWorkspaces
                 , focusFollowsMouse = False
 		}
  
  
- 
---logHook
-myLogHook :: Handle -> X ()
-myLogHook h = dynamicLogWithPP $ customPP { ppOutput = hPutStrLn h }
- 
- 
- 
----- Looks --
----- bar
-customPP :: PP
-customPP = defaultPP { 
-     			    ppHidden = xmobarColor "#00FF00" ""
-			  , ppCurrent = xmobarColor "#FF0000" "" . wrap "[" "]"
-			  , ppUrgent = xmobarColor "#FF0000" "" . wrap "*" "*"
-                          , ppLayout = xmobarColor "#FF0000" ""
-                          , ppTitle = xmobarColor "#00FF00" "" . shorten 80
-                          , ppSep = "<fc=#0033FF> | </fc>"
-                     }
- 
--- some nice colors for the prompt windows to match the dzen status bar.
-myXPConfig = defaultXPConfig                                    
-    { 
-	font  = "-*-terminus-*-*-*-*-12-*-*-*-*-*-*-u" 
-	,fgColor = "#00FFFF"
-	, bgColor = "#000000"
-	, bgHLight    = "#000000"
-	, fgHLight    = "#FF0000"
-	, position = Top
-    }
- 
--------------------------------------------------------------------------------
----- Terminal --
-myTerminal :: String
-myTerminal = "urxvt"
- 
--------------------------------------------------------------------------------
--- Keys/Button bindings --
--- modmask
-myModMask :: KeyMask
-myModMask = mod4Mask
- 
--- borders
-myBorderWidth :: Dimension
-myBorderWidth = 1
---  
-myNormalBorderColor, myFocusedBorderColor :: String
-myNormalBorderColor = "#333333"
-myFocusedBorderColor = "#FF0000"
---
 -- keys
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     -- killing programs
     [ ((modMask, xK_Return), spawn $ XMonad.terminal conf)
     , ((modMask .|. shiftMask, xK_c ), kill)
- 
-    -- opening program launcher / search engine
-    , ((modMask , xK_s ), SM.submap $ searchEngineMap $ S.promptSearchBrowser myXPConfig "chromium")
-    , ((modMask , xK_p), shellPrompt myXPConfig)
- 
- 
-    -- GridSelect
-    , ((modMask, xK_g), goToSelected defaultGSConfig)
- 
-    -- layouts
-    , ((modMask, xK_space ), sendMessage NextLayout)
-    , ((modMask .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
- 
-    -- focus
-    , ((modMask, xK_Tab ), windows W.focusDown)
-    , ((modMask, xK_j ), windows W.focusDown)
-    , ((modMask, xK_k ), windows W.focusUp)
-    , ((modMask, xK_m ), windows W.focusMaster)
- 
-    -- swapping
-    , ((modMask .|. shiftMask, xK_Return), windows W.swapMaster)
-    , ((modMask .|. shiftMask, xK_j ), windows W.swapDown )
-    , ((modMask .|. shiftMask, xK_k ), windows W.swapUp )
- 
-    -- increase or decrease number of windows in the master area
-    , ((modMask , xK_comma ), sendMessage (IncMasterN 1))
-    , ((modMask , xK_period), sendMessage (IncMasterN (-1)))
- 
-    -- resizing
-    , ((modMask, xK_h ), sendMessage Shrink)
-    , ((modMask, xK_l ), sendMessage Expand)
-    , ((modMask .|. shiftMask, xK_h ), sendMessage MirrorShrink)
-    , ((modMask .|. shiftMask, xK_l ), sendMessage MirrorExpand)
 
-    -- start software 
-    , ((modMask .|.  shiftMask, xK_p ), spawn "pidgin")
-    , ((modMask .|.  shiftMask, xK_b ), spawn "google-chrome")
- 
     -- quit, or restart
     , ((modMask .|. shiftMask, xK_q ), io (exitWith ExitSuccess))
     , ((modMask , xK_q ), restart "xmonad" True)
